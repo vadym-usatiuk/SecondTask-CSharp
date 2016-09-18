c9723057d43adeef4d327bef24d5c5cd4a25b1cb
Vadym Usatiuk <vadym.usatiuk@gmail.com>
2016-09-18 15:25:47 +0300

    Second commit

--- SecondTaskCSharp/Program.cs
+++ SecondTaskCSharp/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
 namespace SecondTaskCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            DateTime start = DateTime.Now;
+            StreamReader sr = new StreamReader(@"../../triangle.txt");
+            List<List<int>> triangle = new List<List<int>>();
+            //1
+            for (int i = 0; i < 100; i++)
+            {
+                string s = sr.ReadLine();
+                char[] c = { ' ' };
+                string[] sa = s.Split(c);
+                List<int> line = new List<int>();
+                foreach (string s1 in sa)
+                    line.Add(int.Parse(s1));
+                triangle.Add(line);
+            }
+            for (int i = 1; i < 100; i++)
+
+                for (int j = 0; j <= i; j++)
+                    if (j == 0)
+                        triangle[i][0] = triangle[i][0] + triangle[i - 1][0];
+                    else if (j == triangle[i].Count - 1)
+                        triangle[i][triangle[i].Count - 1] = triangle[i][triangle[i].Count - 1] +
+                                                             triangle[i - 1][triangle[i - 1].Count - 1];
+                    else
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i][j] > triangle[i - 1][j] + triangle[i][j]
+                                             ? triangle[i - 1][j - 1] + triangle[i][j]
+                                             : triangle[i - 1][j] + triangle[i][j];
+            int max = 0;
+            foreach (int i in triangle[triangle.Count - 1])
+                max = i > max ? i : max;
+            Console.WriteLine(max);
+            Console.ReadKey();
         }
     }
 }
